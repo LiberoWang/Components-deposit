75da47ee1d91981f1c9af4ed79bd062c34da084c
luokui <luokui@helijia.com>
2020-08-14 22:20:17 +0800

    框架

---
+++ src/Toast.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Toast extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>点击我toast提示</div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Toast;
