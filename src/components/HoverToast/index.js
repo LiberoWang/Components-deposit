30618f8a5301a084634986117365c7a52069bb64
luokui <luokui@helijia.com>
2020-08-18 17:13:55 +0800

    使用useEffect

--- src/components/HoverToast/index.js
+++ src/components/HoverToast/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import ToastContent from './ToastContent';
+import React, { useEffect } from 'react';
+import cx from 'classnames';
+import { CSSTransition } from 'react-transition-group';
 import './index.scss'
 
 
 const HoverToastWidget = ({ msg, duration = 2000, visible, children, onCancel }) => {
-  const notice = { content: msg, duration };
+  useEffect(() => {
+    if (visible) {
+      setTimeout(() => {
+        onCancel();
+      }, duration);
+    }
+  }, [visible]);
+
   return(
     <div className="hover-taost">
       { children }
-      { visible &&
-        <ToastContent notice={notice} onCancel={onCancel}/>
-      }
+      <div className={cx('hidden', {'nomarl': visible })}>
+        <CSSTransition
+          in={visible}
+          classNames="toast-notice-hover notice"
+          timeout={300}
+        >
+          <div className="toast-notice">{ msg }</div>
+        </CSSTransition>
+      </div>
     </div>
   );
 };
